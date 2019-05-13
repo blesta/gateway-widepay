d64d9a05683ec19ffa86706aea5c7c7393201aa4
Temporary <Temporary@Paul-XPS>
2019-05-13 14:41:07 -0700

    Added Portuguese language file

---
+++ language/pt_br/widepay.php
@@ -0,0 +1,12 @@
+<?php
+$lang['Widepay.name'] = 'Wide Pay';
+$lang['Widepay.wallet_id'] = 'ID de Carteira';
+$lang['Widepay.wallet_token'] = 'Token de Carteira';
+
+$lang['Widepay.!error.wallet_id.format'] = 'Por favor, insira um ID da carteira';
+$lang['Widepay.!error.wallet_token.format'] = 'Por favor, insira um token de carteira da API';
+
+$lang['Widepay.buildprocess.submit'] = 'Pague com Wide Pay';
+
+$lang['Widepay.charge_types.ticket'] = 'Boleto';
+$lang['Widepay.charge_types.card'] = 'Cartão';
